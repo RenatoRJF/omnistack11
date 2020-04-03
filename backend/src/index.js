1c9299aa2a6924b917564ae098debfef3fcf9c1f
Renato Ferreira <nato.rjf@gmail.com>
2020-04-03 19:41:25 +0200

    Backend of application

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use('/api', routes);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
